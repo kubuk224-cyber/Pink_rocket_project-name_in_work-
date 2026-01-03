a4bacccc08cfbd7b271729259ae26ea63f2f48eb
kubuk224-cyber <kubuk224@gmail.com>
2026-01-03 15:23:10 +0100

    day one patch

--- Assets/player/code/PlayerController.cs
+++ Assets/player/code/PlayerController.cs
@@ -1,20 +1,35 @@
 using UnityEngine;
 
+// To sprawi, że Unity samo doda Rigidbody2D, jeśli zapomnisz
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    public float moveSpeed = 5f;
     public float hInput;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    private Rigidbody2D rb;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
+        // Zabezpieczenia fizyki w kodzie (dla pewności)
+        rb.gravityScale = 0; // Wyłączamy grawitację (żeby nie spadał)
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation; // Blokujemy obracanie się
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous; // Lepsze wykrywanie kolizji
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Pobieramy wejście w Update (tutaj jest najpłynniej)
         hInput = Input.GetAxis("Horizontal");
+    }
 
-        transform.Translate(Vector2.right * hInput *moveSpeed* Time.deltaTime);
+    // Fizykę zawsze wykonujemy w FixedUpdate
+    void FixedUpdate()
+    {
+        // Ustawiamy prędkość (Velocity).
+        // Dzięki temu, gdy uderzymy w ścianę, silnik fizyczny nas zatrzyma.
+        rb.linearVelocity = new Vector2(hInput * moveSpeed, 0); 
     }
 }
